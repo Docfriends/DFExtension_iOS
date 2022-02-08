530581160858dc18d2393073513f4af3811d30e2
zehye <zehye.01@gmail.com>
2022-02-08 11:21:50 +0900

    UIColor 코드 변경

--- df-extension-ios/Classes/UI/UIColor+.swift
+++ df-extension-ios/Classes/UI/UIColor+.swift
@@ -29,19 +29,25 @@ public extension UIColor {
      - parameter hex: String
      */
     convenience init(hexString: String) {
-        let scanner = Scanner(string: hexString)
-        scanner.scanLocation = 0
-        var rgbValue: UInt64 = 0
-        scanner.scanHexInt64(&rgbValue)
         
-        let r = (rgbValue & 0xff0000) >> 16
-        let g = (rgbValue & 0xff00) >> 8
-        let b = rgbValue & 0xff
-        self.init(
-            red: CGFloat(r) / 0xff,
-            green: CGFloat(g) / 0xff,
-            blue: CGFloat(b) / 0xff, alpha: 1
-        )
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        
+        switch hex.count {
+        case 3:
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8:
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
     }
     
     /**
